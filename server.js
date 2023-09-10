655efd65d9f0c80aec63f895d146904675ab4416
dheeru2504 <138882915+dheeru2504@users.noreply.github.com>
2023-09-11 01:24:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 
 
 const corsOptions ={
-  origin:'https://e-commerce-frontend-nu-nine.vercel.app/', 
+  origin:'https://e-commerce-frontend-nu-nine.vercel.app', 
   method:"GET,POST,PUT,DELETE",
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,

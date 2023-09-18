3842ede0b306f38fd724a19556bf752da6492f34
dheeru2504 <138882915+dheeru2504@users.noreply.github.com>
2023-09-19 02:22:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: 'https://front-dv26.onrender.com',
+  origin: '*',
   method: "GET,POST,PUT,DELETE",
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200,

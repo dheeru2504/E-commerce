15579658d7c8918f70fc65394654a74d221da0fb
dheeru2504 <138882915+dheeru2504@users.noreply.github.com>
2023-09-19 02:48:10 +0530

    Update CategoryProduct.js

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -12,7 +12,7 @@ const CategoryProduct = () => {
   const getPrductsByCat = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/product-category/${params.slug}`
+        `https://backend-k2am.onrender.com/api/v1/product/product-category/${params.slug}`
       );
       setProducts(data?.products);
       setCategory(data?.category);
@@ -36,7 +36,7 @@ const CategoryProduct = () => {
               {products?.map((p) => (
                 <div className="card m-2" key={p._id}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://backend-k2am.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />

000738ebb015cc4720205b02d33b6a03c113e350
dheeru2504 <138882915+dheeru2504@users.noreply.github.com>
2023-09-19 02:49:54 +0530

    Update Orders.js

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -12,7 +12,7 @@ const Orders = () => {
   const [auth, setAuth] = useAuth();
   const getOrders = async () => {
     try {
-      const { data } = await axios.get("/api/v1/auth/orders");
+      const { data } = await axios.get("https://backend-k2am.onrender.com/api/v1/auth/orders");
       setOrders(data);
     } catch (error) {
       //console.log(error);
@@ -70,7 +70,7 @@ const Orders = () => {
                       <div className="card-body orders-card-body" key={p._id}>
                         <div className="">
                           <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
+                            src={`https://backend-k2am.onrender.com/api/v1/product/product-photo/${p._id}`}
                             className="card-img-top"
                             alt={p.name}
                             width="100px"
